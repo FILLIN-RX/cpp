2b3711d2f04c98ecc5c07d2cf735b9d74ce32e36
RUXEL <djeutchouruxel@gmail.com>
2025-06-15 01:43:56 +0100

    Testing the  article class

--- tp 102/projet1.cpp
+++ tp 102/projet1.cpp
@@ -19,6 +19,9 @@ public:
     double getPrix(){
         return prix;
     };
+    double getTva(){
+        return tva;
+    };
 
     void setReference(string ref){
         reference = ref;
@@ -29,24 +32,52 @@ public:
     void setPrix(double prix){
         this->prix = prix;
     };
+    void setTva(double Tva){
+        tva = Tva;
+    };
     Article():reference("N/A"),designation("N/A"),prix(0.0){};
-    Article(string ref,string des,double prix){
+    Article(string ref,string des,double prix):reference(ref),designation(des),prix(prix){
+        // Constructeur avec paramètres
+        // Initialise l'article avec une référence, une désignation et un prix spécifiques
+    }
+    Article(string ref,string des):reference(ref),designation(des),prix(0.0){
+        // Constructeur avec paramètres
+        // Initialise l'article avec une référence, une désignation et un prix spécifiques
         
     }
-    void calculerPrixTTC(){
-        double prixTTC = prix + (prix * tva / 100);
-        
+    Article(const Article& autre)
+    : reference(autre.reference), designation(autre.designation), prix(autre.prix) {}
 
+    double calculerPrixTTC(){
+        double prixTTC = prix + (prix * tva / 100);
+        return prixTTC;
     }
-    void afficher(double prixTTC){
+    void afficher(){
         cout << "Détails de l'article :" << endl;
         cout << "------------------------" << endl;
         cout << "Reference:" << reference << endl;
         cout << "Designation:" << designation << endl;
         cout << "Prix HT:" << prix << endl;
         cout << "TVA:"<<tva<<"%"<<endl;
-        cout << "Prix TTC de l'article " << designation << " (Réf: " << reference << ") : " << prixTTC << endl;
+        cout << "Prix TTC de l'article : " << calculerPrixTTC() << "fcfa" << endl;
     }
 };
+double Article::tva = 18.0; // ou la valeur que vous souhaitez
+int main(){
+    Article a1("A001", "Article 1", 100.0);
+    Article a2("A002", "Article 2");
 
+    a1.afficher();
+    a2.afficher();
+    a1.setTva(50);
+    a1.afficher();
+    a2.setTva(50);
+    a2.afficher();
+    a2.setPrix(200.0);
+    a2.afficher();
+    Article a3 = a1; // Utilisation du constructeur de copie
+    cout << "Détails de l'article copié :" << endl;
+    a3.afficher();
+    return 0;
+}
 
